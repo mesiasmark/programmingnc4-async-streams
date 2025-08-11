4c0b08e236004c49fb7c2568161da1e311cd0929
Jayson-prog <javerdomaraya7@gmail.com>
2025-08-11 23:13:03 +0800

    Add Part 2: Error handling in Future

---
+++ lib/part2_error_handling.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+Future<void> simulateError() async {
+  await Future.delayed(Duration(seconds: 2));
+  throw Exception("Something went wrong in Future!");
+}
+
+Future<void> main() async {
+  try {
+    await simulateError();
+  } catch (e) {
+    print("Caught an error: $e");
+  }
+}
